196d83b47dda45bbe5654f40adba68a681421e3e
Mosesbwire <mosesdev92@gmail.com>
2024-10-01 00:37:43 +0300

    feat: add notification functionality
    - add notification service
    
     -add mailchimp email provider

--- app/services/messaging_service_provider.py
+++ app/services/messaging_service_provider.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+import mailchimp_transactional as MailChimpTransactional
+from mailchimp_transactional.api_client import ApiClientError
+
 
 class MessagingServiceProvider(ABC):
 
@@ -8,7 +11,43 @@ class MessagingServiceProvider(ABC):
     
 
 class MailChimpEmailMessagingService(MessagingServiceProvider):
-    pass
+
+    def __init__(self, api_key:str, sender_address:str):
+        self.api_key = api_key
+        self.sender = sender_address
+        self._client = None
+    
+    def init(self):
+        try:
+            self.client = MailChimpTransactional.Client(self.api_key)
+        except ApiClientError as err:
+            print(f'An exception occured {err.text}')
+
+    @property
+    def client(self):
+        return self._client
+    
+    @client.setter
+    def client(self, mailChimp):
+        self._client = mailChimp
+    
+    def send(self, recepient: str, body: str, subject: str):
+        
+        msg = {
+            "from_email": self.sender,
+            "subject": subject,
+            "text": body,
+            "to": [
+                {
+                    "email": recepient,
+                    "type": "to"
+                }
+            ]
+        }
+        
+        resp = self.client.messages.send({"message":msg})
+        
+        
 
 class TwilioSmsMessagingService(MessagingServiceProvider):
     pass
--- app/services/notification.py
+++ app/services/notification.py
@@ -1,9 +1,21 @@
-from .messaging_service_provider import MessagingServiceProvider 
+from .messaging_service_provider import MessagingServiceProvider, MailChimpEmailMessagingService
 from abc import ABC, abstractmethod
+import os
 from typing import TypeVar
 
 
 class Notification(ABC):
+    """
+    Abstract base class for notification services.
+
+    This class defines a common interface for sending notifications using different messaging
+    service providers. Subclasses must implement the `notify` method to define how notifications
+    are sent.
+
+    Attributes:
+        messagingServiceProvider (MessagingServiceProvider): An instance of a messaging service provider
+            that handles sending notifications. This must be provided by subclasses.
+    """
     def __init__(self, messagingServiceProvider:MessagingServiceProvider)-> None:
         self.messagingServiceProvider = messagingServiceProvider
 
@@ -13,12 +25,59 @@ class Notification(ABC):
 
 
 class EmailNotification(Notification):
+    """
+    Implementation of the Notification class responsible for sending email notifications.
+
+    This class uses a specified messaging service provider to send email notifications.
+
+    Attributes:
+        messagingServiceProvider (MessagingServiceProvider): An instance of a messaging service
+            provider that handles sending emails. This can be any class that implements the
+            MessagingServiceProvider interface.
+    """
     def __init__(self, messagingServiceProvider:MessagingServiceProvider) -> None:
+        """
+        Initializes the EmailNotification service with a specified messaging service provider.
+
+        Args:
+            messagingServiceProvider (MessagingServiceProvider): A service provider for sending
+                messages, implementing the MessagingServiceProvider interface.
+        """
         super().__init__(messagingServiceProvider)
     
 
     def notify(self, recepient: str, message: str, subject: str):
+        """
+        Sends an email notification to the specified recipient.
+
+        Args:
+            recipient (str): The email address of the recipient.
+            message (str): The body content of the email.
+            subject (str): The subject line of the email.
+        """
         self.messagingServiceProvider.send(recepient, message, subject)
+
+
+def get_email_notification_service(sender_address: str):
+    """
+    Initializes and returns an email notification service using MailChimp's transactional API.
+
+    Args:
+        sender_address (str): The email address that will be used as the sender for outgoing emails.
+        example: registration@scholafit.com
+    Returns:
+        EmailNotification: An instance of the EmailNotification class initialized with 
+                           the MailChimp email messaging client.
+    
+    Environment Variables:
+        MAILCHIMP_TRANSACTIONAL_API_KEY: API key for MailChimp's transactional email service, 
+                                         retrieved from the environment variables.
+    """
+
+    API_KEY = os.getenv('MAILCHIMP_TRANSACTIONAL_API_KEY')
+    client = MailChimpEmailMessagingService(API_KEY, sender_address)
+    client.init()
+    return EmailNotification(client)
 
 
 class SMSNotification(Notification):
@@ -27,4 +86,6 @@ class SMSNotification(Notification):
     
 
     def notify(self, recepient: str, message: str):
+        
         self.messagingServiceProvider.send(recepient, message)
+
