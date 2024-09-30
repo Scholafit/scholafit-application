fbfb1a31b84b0195a82accc4a35c9c90e495c46d
Mosesbwire <mosesdev92@gmail.com>
2024-09-30 08:25:03 +0300

    feat: Add models for the learning module
    - Adds capability to store test questions, subjects, user answers, test results

--- app/models/learning_module/question.py
+++ app/models/learning_module/question.py
@@ -1,30 +1,100 @@
+
 from app.models.base_model import BaseModel
+from app.models.database import Repository
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Text, ForeignKey, Integer, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import List
 
 
 
-class Question(BaseModel):
+class DB_Question(BaseModel):
 
     __tablename__ = "questions"
 
-    content: Mapped[str] = mapped_column(Text, nullable=False)
+    question: Mapped[str] = mapped_column(Text, nullable=False)
     answer_explanation: Mapped[str] = mapped_column(Text, nullable=False)
     is_pregenerated: Mapped[bool] = mapped_column(Boolean, nullable=False)
     subject_id: Mapped[int] = mapped_column(Integer, ForeignKey("subjects.id"), nullable=False)
+    passage_id: Mapped[int] = mapped_column(Integer, ForeignKey("passages.id"), nullable=True)
 
-    subject: Mapped["Subject"] = relationship(back_populates="questions")
-    answer_choices:Mapped[List["QuestionAnswerChoices"]] = relationship(back_populates="question", cascade="all, delete-orphan")
+    passage: Mapped["DB_Passage"] = relationship(back_populates="questions")
+    subject: Mapped["DB_Subject"] = relationship(back_populates="questions")
+    answer_choices:Mapped[List["DB_QuestionAnswerChoices"]] = relationship(back_populates="question", cascade="all, delete-orphan")
+
+    def __init__(self, *args, **kwargs):
+        self.passage_id = kwargs.get('passage_id', None)
+        self.question = kwargs.get("question")
+        self.answer_explanation = kwargs.get("explanation")
+        self.is_pregenerated = True
+        self.subject_id = kwargs.get("subject_id")
 
 
+class QuestionRepository(Repository):
+    def __init__(self, database: SQLAlchemy):
+        super().__init__(database)
 
 
-class QuestionAnswerChoices(BaseModel):
+class Question:
+    def __init__(self, questionRepository: Repository):
+        self.db = questionRepository
+
+
+    def create_question(self, **kwargs):
+        question = DB_Question(**kwargs)
+        return self.db.save(question).to_dict()
+
+
+class DB_QuestionAnswerChoices(BaseModel):
 
     __tablename__ = "question_choices"
 
     answer_text: Mapped[str] = mapped_column(Text, nullable=False)
     is_correct: Mapped[bool] = mapped_column(Boolean, nullable=False)
     question_id: Mapped[int] = mapped_column(Integer, ForeignKey("questions.id"), nullable=False)
-    question: Mapped["Question"] = relationship(back_populates="answer_choices")
+    question: Mapped["DB_Question"] = relationship(back_populates="answer_choices")
+
+    def __init__(self, answer, is_correct, question_id):
+        self.answer_text = answer
+        self.is_correct = is_correct
+        self.question_id = question_id
+
+
+class QuestionAnswerRepository(Repository):
+    def __init__(self, database: SQLAlchemy):
+        super().__init__(database)
+
+
+class QuestionAnswer:
+    def __init__(self, questionAnswerRepository: Repository):
+        self.db = questionAnswerRepository
+
+
+    def create_answer(self, answer, is_correct, question_id):
+        
+        ans = DB_QuestionAnswerChoices(answer, is_correct, question_id)
+        return self.db.save(ans).to_dict()
+
+
+class DB_Passage(BaseModel):
+    __tablename__ = "passages"
+
+    passage: Mapped[str] = mapped_column(Text, nullable=True)
+    questions: Mapped[List["DB_Question"]] = relationship(back_populates="passage")
+
+    def __init__(self, passage):
+        self.passage = passage
+
+class PassageRepository(Repository):
+    def __init__(self, database: SQLAlchemy):
+        super().__init__(database)
+
+
+class Passage:
+    def __init__(self, repository: Repository) -> None:
+        self.db = repository
+    
+
+    def create_passage(self, passage_text):
+        psg = DB_Passage(passage_text)
+        return self.db.save(psg).to_dict()
--- app/models/learning_module/subject.py
+++ app/models/learning_module/subject.py
@@ -1,14 +1,40 @@
 from app.models.base_model import BaseModel
+from app.models.database import Repository
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean, Integer, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import List
  
 
-class Subject(BaseModel):
+class DB_Subject(BaseModel):
     __tablename__ = "subjects"
     name:Mapped[str] = mapped_column(String(120), nullable=False)
     isEnglish: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    questions: Mapped[List["Question"]] = relationship(back_populates="subject", cascade="all, delete-orphan")
+    questions: Mapped[List["DB_Question"]] = relationship(back_populates="subject", cascade="all, delete-orphan")
+
+    def __init__(self, name: str, isEnglish: bool):
+        self.name = name
+        self.isEnglish = isEnglish
+
+
+class SubjectRepository(Repository):
+     def __init__(self, database: SQLAlchemy):
+        super().__init__(database)
+
+
+
+class Subject:
+    def __init__(self, subjectRepository: Repository):
+        self.db = subjectRepository
+
+
+    def create_subject(self, name: str, isEnglish: bool):
+        subject = DB_Subject(name, isEnglish)
+        return self.db.save(subject).to_dict()
+
+
+
+
 
 
 class UserSubject(BaseModel):
